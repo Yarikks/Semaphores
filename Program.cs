33d30cb732fed28ef3ca52139e0d9b4456f3c755
Yorik <jasper18poil@gmail.com>
2020-03-29 14:26:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Semaphores
+{
+    class Program
+    {
+        static Semaphore semaphore = new Semaphore(1, 5);
+        static Thread[] threads = new Thread[5];
+
+        static void Main(string[] args)
+        {
+            for (int i = 0, k = 1; i < 5; i++, k++)
+            {
+                threads[i] = new Thread(StudentAction);
+                threads[i].Name = $"Студент {k}";
+                threads[i].Start();
+            }
+
+            for(int i = 0; i < 5; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.WriteLine("\nАудитория закрывается.");
+            Console.ReadLine();
+        }
+
+        static private void StudentAction()
+        {
+            semaphore.WaitOne();
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} входит в аудиторию.");
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} учится.");
+            Thread.Sleep(1000);
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} покидает аудиторию.");
+
+            semaphore.Release();
+
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Reader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Semaphores
+{
+    class Reader
+    {
+        static Semaphore sem = new Semaphore(3, 3);
+        Thread myThread;
+
+        public Reader(int i)
+        {
+            myThread = new Thread(Read);
+            myThread.Name = $"Студент {i}";
+            myThread.Start();
+        }
+
+        public void Read()
+        {
+            sem.WaitOne();
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} входит в аудиторию.");
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} учится.");
+            Thread.Sleep(1000);
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} покидает аудиторию.");
+
+            sem.Release();
+
+            Thread.Sleep(1000);
+        }
+    }
+}
